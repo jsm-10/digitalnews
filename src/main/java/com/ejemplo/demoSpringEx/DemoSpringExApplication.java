47cce9547f908684550a977ee006cc8d0f13586a
juans <juans@Juan-Sebastian>
2024-03-10 17:56:13 -0300

    starting a new project

---
+++ src/main/java/com/ejemplo/demoSpringEx/DemoSpringExApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demoSpringEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringExApplication.class, args);
+	}
+
+}
